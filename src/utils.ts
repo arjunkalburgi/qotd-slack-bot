634fbe7dac577bd61373722aab6f21afa8c43d70
Arjun <askalburgi@gmail.com>
2023-02-13 20:57:11 -0500

    bugfix start qotd

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import { ReceiverEvent } from "@slack/bolt";
 import { ChatPostMessageResponse } from '@slack/web-api';
+import fetch from "node-fetch";
 
 export function parseRequestBody(stringBody: string | null, contentType: string | undefined): any | undefined {
     try {
