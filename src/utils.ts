7ffe78a3fef8187f8ab76dd97f5752f366c84a1c
Arjun <askalburgi@gmail.com>
2023-02-17 14:37:29 -0500

    update time

--- src/utils.ts
+++ src/utils.ts
@@ -54,11 +54,11 @@ export const timeOfThen = () => {
     var then = (now.getHours() >= 13) ? 
         new Date(now.getFullYear(), now.getMonth(),
             now.getDate() + 1,  // the next day, ...
-            13, 0, 0            // ... at 13:00:00 hours
+            19, 39, 0            // ... at 13:00:00 hours
         ) : 
         new Date(now.getFullYear(), now.getMonth(), 
             now.getDate(),      // today, ...
-            13, 0, 0            // ... at 13:00:00 hours
+            19, 39, 0            // ... at 13:00:00 hours
         );
 
     return then.getTime();
